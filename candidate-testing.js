8e70dc7a5e6a99dff4a46a1e3d81d4e35bcc0d28
NikKiLearn <85259655+Nikitha-prog@users.noreply.github.com>
2021-06-17 15:37:09 +0000

    Cleaned  the code by removing extra spaces and comments

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,19 +7,14 @@ let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
-
 let candidateAnswer;
 let Status;
 let questionNumber=1;
-
-
 let questions=["Who was the first American woman in space?","True or false: 5 kilometer == 5000 meters?","(5 + 3)/2 * 10 = ?"," Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
-
 let correctAnswers=["Sally Ride"
 ,"true","40"
 ,"Trajectory","3"];
 let candidateAnswers=[];
-//let userAnswer=[];
 
 function askForName() {
    // TODO 1.1b: Ask for candidate's name //
@@ -27,44 +22,18 @@ function askForName() {
  //console.log("Hello "+ candidateName+"!");
 
 }
-
-/*function askQuestion() {
+function askQuestion(){
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-/*console.log("1) "+ question);
-candidateAnswer=input.question("Your Answer: ");
-
-if(candidateAnswer!==correctAnswer)
-{
-  console.log("Correct Answer: "+correctAnswer);}
-}*/
-/*if(candidateAnswer===correctAnswer)
-{
-
-console.log("Correct Answer: "+correctAnswer);
-}
-else {
-  console.log("Correct Answer: "+correctAnswer);
-}*/
-
-  function askQuestion()
-  {
   for(i=0;i<questions.length;i++)
   {
   console.log(`${questionNumber}) ${questions[i]}`);
   questionNumber=questionNumber+1;
   candidateAnswer=input.question(`Your Answer: `);
   candidateAnswers.push(candidateAnswer);
- // console.log("candidateAnswers"+candidateAnswers);
   console.log(`Correct Answer: ${correctAnswers[i]}`);
   console.log("\n");
-
-    }
-
   }
-
-
-
+}
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly
@@ -72,16 +41,15 @@ function gradeQuiz(candidateAnswers) {
  let numberOfQuestions;
  numberOfQuestions=questions.length;
   for(i=0;i<candidateAnswers.length;i++)
-  {
+{
     candidateAnswerslowerCase=candidateAnswers[i].toLowerCase();
     correctAnswersLowercase=correctAnswers[i].toLowerCase();
     if(candidateAnswerslowerCase===correctAnswersLowercase)
-  //if(candidateAnswers[i]===correctAnswers[i])
   {
     numberOfCorrectAnswer=numberOfCorrectAnswer+1;
   }
-  }
-  let grade;
+}
+ let grade;
   grade=(numberOfCorrectAnswer/numberOfQuestions)*100
 console.log(`>>> Overall Grade: ${grade} % (${numberOfCorrectAnswer}  of ${numberOfQuestions} responses correct) <<<`)
 if(grade<80)
@@ -94,10 +62,10 @@ console.log(`>>> Status: PASSED <<<`);
 }
   return grade;
 }
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
